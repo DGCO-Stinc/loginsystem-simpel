d15c6df28f85f06952d7ab52d60f550aaf658786
Rebitteration <g@collette-online.nl>
2020-01-14 00:46:43 +0100

    made dbh into class

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,3 +1,21 @@
 <?php
 
-$dbh = new PDO('mysql:host=localhost;dbname=testlogin','root','');
+class DBConn
+{
+    private $host = "localhost";
+    private $dbname="deb113326_2092822";
+    private $uname= "deb113326_2092822";
+    private $pwd = "-";
+    public $dbconn;
+
+    public function __construct()
+    {
+        $dbconn = new PDO('mysql:host='.$this->host.';dbname='.$this->dbname,$this->uname,$this->pwd);
+
+    }
+
+    public function getConnection()
+    {
+        return $dbconn;
+    }
+}
